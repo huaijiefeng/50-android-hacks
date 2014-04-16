f3799f116a934c88d1f50c4abf6c3f6a4b9aaf35
huibinfeng0810 <huibinfeng0810@gmail.com>
2014-04-16 14:57:06 +0800

    init project

---
+++ android-hacks/src/com/huaijie/android_hacks/ui/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.huaijie.android_hacks.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fenghb on 2014/4/16.
+ */
+public class HomeActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
